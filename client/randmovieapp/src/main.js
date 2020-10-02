5121ef44c80649c3e4e169c5d7caab7c5575e687
cedricscoolmouv <cedric@schoolmouv.fr>
2020-10-02 16:15:38 +0200

    clean adding double quote

--- client/randmovieapp/src/main.js
+++ client/randmovieapp/src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.css"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons"
 
 library.add(faTrashAlt)
 const base = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: "http://localhost:4000",
 })
 
 Vue.prototype.$http = base
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon)
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount('#app')
+}).$mount("#app")
